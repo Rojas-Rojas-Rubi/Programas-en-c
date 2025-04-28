94e6d7a6e72a86fe7554b8f3caec83b622b7df4b
Rojas-Rojas-Rubi <patitomio29@gmail.com>
2025-04-28 07:59:40 -0600

    Codigo de ejemplo

---
+++ Unidad_3/1_Leerscv.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE 1024
+
+int main(){
+    FILE *file;
+    char line [MAX_LINE];
+    char *token;
+
+    //abrir el archivo en modo lectura
+    file = fopen("../1datos.csv", "r");
+    if (file == NULL){
+        perror("Error al abrir el archivo");
+        return 1;
+    }
+    printf("%-15s %-10s %-15s\n", "Nombre", "Edad", "Pais");
+    printf("-----------------------------------------------\n");
+    
+    while (fgets(line, sizeof(line), file)){
+        line[strcspn(line, "\n")] = 0;
+
+        token = strtok(line, ",");
+        if (token != NULL) printf("%-15s ", token);
+
+        token = strtok(NULL, ",");
+        if (token != NULL) printf("%-10s ", token);
+
+        token = strtok(NULL, ",");
+        if (token != NULL) printf("%-15s ", token);
+
+        printf("\n");
+    }
+fclose(file);
+
+return 0;
+}
